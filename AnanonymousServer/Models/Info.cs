29533aa05b4dfe35aff19e5cc5c1b89aa48b9cd1
Ivan Antonov <ivan_antonov@live.ru>
2017-09-21 12:37:44 +0300

    Добавьте файлы проекта.

---
+++ AnanonymousServer/Models/Info.cs
@@ -0,0 +1,9 @@
+﻿namespace AnanonymousServer.Models
+{
+    public class Info
+    {
+        public string Path { get; set; }
+        public string Name { get; set; }
+        public string Data { get; set; }
+    }
+}
